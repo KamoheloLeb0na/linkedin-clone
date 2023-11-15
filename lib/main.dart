432cc69ad7171ceaf571ae2aac25a82a4a46785e
Kamohelo <kamohelolebna@gmail.com>
2023-11-15 16:58:35 +0200

    Initail Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LinkedIn());
+}
+
+class LinkedIn extends StatelessWidget {
+  const LinkedIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello Mother!");
+  }
+}
